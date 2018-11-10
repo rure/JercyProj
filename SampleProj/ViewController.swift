a25279f910d0418613809eb129e8dd243ad8a768
rure <rure1114@gmail.com>
2018-11-10 16:12:59 +0900

    init proj

---
+++ SampleProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleProj
+//
+//  Created by juhee on 2018. 11. 10..
+//  Copyright © 2018년 juhee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
